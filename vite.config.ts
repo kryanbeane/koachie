a0b26c552132ef2e7905675006d2d56f4b3945d0
DominikWawak <dominikwawak@gmail.com>
2024-12-17 20:06:12 +0100

    docker file and workflows + PR_template

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		host: '0.0.0.0', // Expose to all interfaces
+		port: 3000 // Optional: explicitly set port
+	}
 });
