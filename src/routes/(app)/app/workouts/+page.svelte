749f90bf4cfa54eb5cc1eb3a97efc033c5436c02
Bryan Keane <bryan.keane@genesys.com>
2025-01-01 22:09:56 +0000

    feat/76: component-ize the exercise instances for workouts page

--- src/routes/(app)/app/workouts/+page.svelte
+++ src/routes/(app)/app/workouts/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-	import type { Exercise, ExerciseInstance } from '@/schemas/exercises';
 	import * as Card from '$lib/components/ui/card/index.js';
-	import * as Table from '$lib/components/ui/table/index.js';
-	import NoOutlineInput from '@/components/ui/input/no_outline_input.svelte';
-	import SetRow from './_components/set_row.svelte';
-	import SelectExercise from './_components/select_exercise.svelte';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import type { ExerciseInstance } from '@/schemas/exercises';
+	import ExerciseInstanceCard from './_components/exercise_instance_card.svelte';
 
 	let exerciseInstances: ExerciseInstance[] = [
 		{
@@ -14,53 +13,29 @@
 			created_at: new Date().toISOString(),
 			updated_at: new Date().toISOString(),
 			performance: []
-		}
-	];
-
-	let exercises: Exercise[] = [
+		},
 		{
-			id: '085b5a1a-5392-4453-9b2d-27ea7a7db052',
-			name: 'Converging Chest Press',
-			coach_id: '5ce2de01-d885-497f-91b4-85eb6e431b95',
+			id: '13ac9ece-597e-4580-90ea-71284a534d82',
+			client_id: '5ce2de01-d885-497f-91b4-85eb6e431b92',
+			exercise_id: '085b5a1a-5392-4453-9b2d-27ea7a7db052',
 			created_at: new Date().toISOString(),
 			updated_at: new Date().toISOString(),
-			note: 'Use the prime plate-loaded machine',
-			instructions: [
-				'First adjust the seat height so that the handles are at chest height.',
-				'Then sit on the seat and grasp the handles with a neutral grip.',
-				'Push the handles together hard until your arms are fully extended.',
-				'Slowly return to the starting position.'
-			],
-			muscle_groups: ['Chest', 'Triceps'],
-			movement_type: 'Horizontal Push',
-			video: 'https://www.youtube.com/watch?v=3VcKaXpzqRo'
+			performance: []
 		}
 	];
-
-	function getExerciseById(exerciseId: string): Exercise | undefined {
-		return exercises.find((exercise) => exercise.id === exerciseId);
-	}
 </script>
 
-<Card.Root class="m-4 w-96 bg-muted/50 outline-double outline-white/25">
-	<Card.Content>
-		<SelectExercise {exercises} />
+<Card.Root class="flex h-full w-full flex-col">
+	<Card.Content class="flex flex-grow flex-col p-4">
+		<Input type="text" placeholder="Enter text here..." class="mb-4" />
 
-		<Table.Root>
-			<Table.Header>
-				<Table.Row>
-					<Table.Head class="flex-1">Set</Table.Head>
-					<Table.Head class="flex-1">Weight (kg)</Table.Head>
-					<Table.Head class="flex-1">Reps</Table.Head>
-					<Table.Head class="flex-1">RIR</Table.Head>
-					<Table.Head class="flex-1">Rest</Table.Head>
-				</Table.Row>
-			</Table.Header>
-			<Table.Body>
-				<SetRow />
-			</Table.Body>
-		</Table.Root>
+		{#each exerciseInstances as exercise_instance}
+			<ExerciseInstanceCard {exercise_instance} />
+		{/each}
 
-		<NoOutlineInput type="text" placeholder="Enter notes here..." class="mt-2 w-full" />
+		<div class="flex justify-end space-x-2">
+			<Button variant="outline">Cancel</Button>
+			<Button variant="default">Submit</Button>
+		</div>
 	</Card.Content>
 </Card.Root>
---
+++ src/routes/(app)/app/workouts/_components/exercise_instance_card.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import type { Exercise } from '@/schemas/exercises';
+	import * as Card from '$lib/components/ui/card/index.js';
+	import * as Table from '$lib/components/ui/table/index.js';
+	import NoOutlineInput from './no_outline_input.svelte';
+	import SelectExercise from './select_exercise.svelte';
+	import SetRow from './set_row.svelte';
+
+	let { exercise_instance: ExerciseInstance } = $props();
+
+	let exercises: Exercise[] = [
+		{
+			id: '085b5a1a-5392-4453-9b2d-27ea7a7db052',
+			name: 'Converging Chest Press',
+			coach_id: '5ce2de01-d885-497f-91b4-85eb6e431b95',
+			created_at: new Date().toISOString(),
+			updated_at: new Date().toISOString(),
+			note: 'Use the prime plate-loaded machine',
+			instructions: [
+				'First adjust the seat height so that the handles are at chest height.',
+				'Then sit on the seat and grasp the handles with a neutral grip.',
+				'Push the handles together hard until your arms are fully extended.',
+				'Slowly return to the starting position.'
+			],
+			muscle_groups: ['Chest', 'Triceps'],
+			movement_type: 'Horizontal Push',
+			video: 'https://www.youtube.com/watch?v=3VcKaXpzqRo'
+		}
+	];
+
+	function getExerciseById(exerciseId: string): Exercise | undefined {
+		return exercises.find((exercise) => exercise.id === exerciseId);
+	}
+</script>
+
+<Card.Root class="m-4 w-96 bg-muted/50 outline-double outline-white/25">
+	<Card.Content>
+		<SelectExercise {exercises} />
+
+		<Table.Root>
+			<Table.Header>
+				<Table.Row>
+					<Table.Head class="flex-1">Set</Table.Head>
+					<Table.Head class="flex-1">Weight (kg)</Table.Head>
+					<Table.Head class="flex-1">Reps</Table.Head>
+					<Table.Head class="flex-1">RIR</Table.Head>
+					<Table.Head class="flex-1">Rest</Table.Head>
+				</Table.Row>
+			</Table.Header>
+			<Table.Body>
+				<SetRow />
+			</Table.Body>
+		</Table.Root>
+
+		<NoOutlineInput type="text" placeholder="Enter notes here..." class="mt-2 w-full" />
+	</Card.Content>
+</Card.Root>
