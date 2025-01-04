823d980ab428a21e8ce5eb8980913edb8168938d
Bryan Keane <bryankeane0@gmail.com>
2025-01-04 22:49:47 +0000

    feat/74: added vercel deploy action and workout table

--- src/routes/(app)/app/workouts/(components)/workout_card.svelte
+++ src/routes/(app)/app/workouts/(components)/workout_card.svelte
@@ -1,11 +1,30 @@
 <script lang="ts">
-	import * as Card from '$lib/components/ui/card/index.js';
 	import { Input } from '$lib/components/ui/input/index.js';
-	import { Button } from '$lib/components/ui/button/index.js';
 	import type { ExerciseInstance } from '@/schemas/exercises';
 	import ExerciseInstanceCard from './exercise_instance_card.svelte';
+	import ScrollArea from '@/components/ui/scroll-area/scroll-area.svelte';
+
+	export let workout;
 
 	let exerciseInstances: ExerciseInstance[] = [
+		{
+			id: '13ac9ece-597e-4580-90ea-71284a534d87',
+			client_id: '5ce2de01-d885-497f-91b4-85eb6e431b95',
+			exercise_id: '085b5a1a-5392-4453-9b2d-27ea7a7db052',
+			workout_id: '13ac9ece-597e-4580-90ea-71284a534d82',
+			created_at: new Date().toISOString(),
+			updated_at: new Date().toISOString(),
+			performance: []
+		},
+		{
+			id: '13ac9ece-597e-4580-90ea-71284a534d82',
+			client_id: '5ce2de01-d885-497f-91b4-85eb6e431b92',
+			exercise_id: '085b5a1a-5392-4453-9b2d-27ea7a7db052',
+			workout_id: '13ac9ece-597e-4580-90ea-71284a534d82',
+			created_at: new Date().toISOString(),
+			updated_at: new Date().toISOString(),
+			performance: []
+		},
 		{
 			id: '13ac9ece-597e-4580-90ea-71284a534d87',
 			client_id: '5ce2de01-d885-497f-91b4-85eb6e431b95',
@@ -27,8 +46,13 @@
 	];
 </script>
 
-<Input type="text" placeholder="Enter text here..." class="mb-4" />
-
-{#each exerciseInstances as exercise_instance}
-	<ExerciseInstanceCard {exercise_instance} />
-{/each}
+<div class="flex h-full flex-col">
+	<Input type="text" placeholder={workout.name} class="m-4" />
+	<div class="flex-grow overflow-y-auto">
+		<ScrollArea orientation="vertical">
+			{#each exerciseInstances as exercise_instance}
+				<ExerciseInstanceCard {exercise_instance} />
+			{/each}
+		</ScrollArea>
+	</div>
+</div>
