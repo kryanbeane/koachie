da12cb78235fef522b6fa00429678d8fc6f2890a
Bryan Keane <bryankeane0@gmail.com>
2025-01-05 19:55:20 +0000

    feat/74: fix layout scrolly behaviour

--- src/routes/(app)/app/workouts/(components)/workout_card.svelte
+++ src/routes/(app)/app/workouts/(components)/workout_card.svelte
@@ -35,8 +35,8 @@
 			performance: []
 		},
 		{
-			id: '13ac9ece-597e-4580-90ea-71284a534d82',
-			client_id: '5ce2de01-d885-497f-91b4-85eb6e431b92',
+			id: '13ac9ece-597e-4580-90ea-71284a534d87',
+			client_id: '5ce2de01-d885-497f-91b4-85eb6e431b95',
 			exercise_id: '085b5a1a-5392-4453-9b2d-27ea7a7db052',
 			workout_id: '13ac9ece-597e-4580-90ea-71284a534d82',
 			created_at: new Date().toISOString(),
@@ -46,13 +46,9 @@
 	];
 </script>
 
-<div class="flex h-full flex-col">
-	<Input type="text" placeholder={workout.name} class="m-4" />
-	<div class="flex-grow overflow-y-auto">
-		<ScrollArea orientation="vertical">
-			{#each exerciseInstances as exercise_instance}
-				<ExerciseInstanceCard {exercise_instance} />
-			{/each}
-		</ScrollArea>
-	</div>
-</div>
+<Input type="text" placeholder={workout.name} class="m-4" />
+<ScrollArea orientation="vertical">
+	{#each exerciseInstances as exercise_instance}
+		<ExerciseInstanceCard {exercise_instance} />
+	{/each}
+</ScrollArea>
