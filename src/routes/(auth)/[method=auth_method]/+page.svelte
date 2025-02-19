948c478919dc690a10ea0d9d7eac8c7a5f9c1a2c
DominikWawak <dominikwawak@gmail.com>
2025-02-19 13:52:46 +0100

    login page better centered

--- src/routes/(auth)/[method=auth_method]/+page.svelte
+++ src/routes/(auth)/[method=auth_method]/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-	import type { PageServerData } from './$types';
+	import type { PageServerData } from "./$types";
 
-	import ArrowLeftIcon from 'lucide-svelte/icons/arrow-left';
+	import ArrowLeftIcon from "lucide-svelte/icons/arrow-left";
 
-	import { historyBack } from '@/components/actions';
-	import { BrandedSplitScreen } from '@/components/layouts';
-	import { buttonVariants } from '@/components/ui/button';
-	import { cn } from '@/utils';
+	import { historyBack } from "@/components/actions";
+	import { BrandedSplitScreen } from "@/components/layouts";
+	import { buttonVariants } from "@/components/ui/button";
+	import { cn } from "@/utils";
 
-	import AuthForm from '../auth/auth-form.svelte';
+	import AuthForm from "../auth/auth-form.svelte";
 
 	interface Props {
 		data: PageServerData;
@@ -21,8 +21,8 @@
 <div class="absolute left-3 top-3 z-50">
 	<button
 		class={cn(
-			buttonVariants({ variant: 'ghost', size: 'icon' }),
-			'rounded-full text-muted-foreground'
+			buttonVariants({ variant: "ghost", size: "icon" }),
+			"rounded-full text-muted-foreground"
 		)}
 		use:historyBack
 	>
@@ -30,6 +30,12 @@
 	</button>
 </div>
 
-<BrandedSplitScreen>
+<!-- <BrandedSplitScreen>
 	<AuthForm {method} form={emailAuthForm} />
-</BrandedSplitScreen>
+</BrandedSplitScreen> -->
+
+<div class="flex h-screen w-screen items-center justify-center">
+	<div class="w-full max-w-md rounded-lg p-6 shadow-lg">
+		<AuthForm {method} form={emailAuthForm} />
+	</div>
+</div>
